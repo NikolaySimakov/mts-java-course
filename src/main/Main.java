f865bdf7a9b14a16dd8d20a352056e370ed1ae88
NikolaySimakov <n.simakoff@gmail.com>
2023-12-13 16:49:54 +0300

    Remove logic from main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,26 +1,7 @@
+package main;
 
 public class Main {
-    public static void calculateTotal(Purchase purchase) {
-        double totalWithoutDiscount = purchase.getQuantity() * purchase.getPrice();
-        double totalWithDiscount = totalWithoutDiscount * (1 - purchase.getDiscount());
-        System.out.println("Общая сумма покупки без скидки: " + String.format("%.2f", totalWithoutDiscount));
-        System.out.println("Сумма со скидкой: " + String.format("%.2f", totalWithDiscount));
-        System.out.println();
-    }
-
     public static void main(String[] args) {
-
-        // 1)
-        Purchase purchase1 = new Purchase(10, 25.50, 0.75);
-        calculateTotal(purchase1);
-
-        // 2)
-        Purchase purchase2 = new Purchase(6, 100.25, 42.575);
-        calculateTotal(purchase2);
-
-        // 3)
-        Purchase purchase3 = new Purchase(19, 15.75, 59.1);
-        calculateTotal(purchase3);
 
     }
 }
