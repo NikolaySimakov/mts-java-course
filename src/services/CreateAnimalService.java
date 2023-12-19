18557aebced56139eadeeb39ea1d707758f8e610
NikolaySimakov <n.simakoff@gmail.com>
2023-12-19 21:50:12 +0300

    int -> BigDecimal

--- src/services/CreateAnimalService.java
+++ src/services/CreateAnimalService.java
@@ -18,19 +18,19 @@ public interface CreateAnimalService {
 
             switch (random.nextInt(4)) {
                 case 0:
-                    Wolf wolf = new Wolf("breed", "name", new BigDecimal(123), "character");
+                    Wolf wolf = new Wolf("breed", "name", new BigDecimal("123.2124512"), "character");
                     System.out.println(wolf.showInfo());
                     break;
                 case 1:
-                    Shark shark = new Shark("breed", "name", new BigDecimal(123), "character");
+                    Shark shark = new Shark("breed", "name", new BigDecimal("287.384727"), "character");
                     System.out.println(shark.showInfo());
                     break;
                 case 2:
-                    Cat cat = new Cat("breed", "name", new BigDecimal(123), "character");
+                    Cat cat = new Cat("breed", "name", new BigDecimal("47839.12847987"), "character");
                     System.out.println(cat.showInfo());
                     break;
                 case 3:
-                    Dog dog = new Dog("breed", "name", new BigDecimal(123), "character");
+                    Dog dog = new Dog("breed", "name", new BigDecimal("758.38749"), "character");
                     System.out.println(dog.showInfo());
                     break;
                 default:
