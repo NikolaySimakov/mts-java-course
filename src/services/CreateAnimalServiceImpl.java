54d8be2f1cede838002e07002fd5a82c72ab2833
NikolaySimakov <n.simakoff@gmail.com>
2023-12-19 23:06:34 +0300

    Remove code duplication

--- src/services/CreateAnimalServiceImpl.java
+++ src/services/CreateAnimalServiceImpl.java
@@ -10,62 +10,43 @@ import java.util.Random;
 
 public class CreateAnimalServiceImpl implements CreateAnimalService {
 
-    public void createAnimalsN(int n) {
-
+    private void chooseRandomAnimal() {
         Random random = new Random();
 
+        switch (random.nextInt(4)) {
+            case 0:
+                Wolf wolf = new Wolf("breed", "name", new BigDecimal("123.2124512"), "character");
+                System.out.println(wolf.showInfo());
+                break;
+            case 1:
+                Shark shark = new Shark("breed", "name", new BigDecimal("287.384727"), "character");
+                System.out.println(shark.showInfo());
+                break;
+            case 2:
+                Cat cat = new Cat("breed", "name", new BigDecimal("47839.12847987"), "character");
+                System.out.println(cat.showInfo());
+                break;
+            case 3:
+                Dog dog = new Dog("breed", "name", new BigDecimal("758.38749"), "character");
+                System.out.println(dog.showInfo());
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void createAnimalsN(int n) {
         for (int i = 0; i < n; i++) {
-            switch (random.nextInt(4)) {
-                case 0:
-                    Wolf wolf = new Wolf("breed", "name", new BigDecimal(123), "character");
-                    System.out.println(wolf.showInfo());
-                    break;
-                case 1:
-                    Shark shark = new Shark("breed", "name", new BigDecimal(123), "character");
-                    System.out.println(shark.showInfo());
-                    break;
-                case 2:
-                    Cat cat = new Cat("breed", "name", new BigDecimal(123), "character");
-                    System.out.println(cat.showInfo());
-                    break;
-                case 3:
-                    Dog dog = new Dog("breed", "name", new BigDecimal(123), "character");
-                    System.out.println(dog.showInfo());
-                    break;
-                default:
-                    break;
-            }
+            this.chooseRandomAnimal();
         }
     }
 
     @Override
     public void createAnimals() {
         int countOfAnimals = 0;
-        Random random = new Random();
 
         do {
-
-            switch (random.nextInt(4)) {
-                case 0:
-                    Wolf wolf = new Wolf("breed", "name", new BigDecimal("123.2124512"), "character");
-                    System.out.println(wolf.showInfo());
-                    break;
-                case 1:
-                    Shark shark = new Shark("breed", "name", new BigDecimal("287.384727"), "character");
-                    System.out.println(shark.showInfo());
-                    break;
-                case 2:
-                    Cat cat = new Cat("breed", "name", new BigDecimal("47839.12847987"), "character");
-                    System.out.println(cat.showInfo());
-                    break;
-                case 3:
-                    Dog dog = new Dog("breed", "name", new BigDecimal("758.38749"), "character");
-                    System.out.println(dog.showInfo());
-                    break;
-                default:
-                    break;
-            }
-
+            this.chooseRandomAnimal();
         } while (++countOfAnimals < 10);
     }
 }
