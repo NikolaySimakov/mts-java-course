d96feacfc78b34436d9b449410e79ca3e09727a4
NikolaySimakov <n.simakoff@gmail.com>
2023-12-19 21:49:45 +0300

    Add Javadocs

--- src/models/Animal.java
+++ src/models/Animal.java
@@ -4,12 +4,28 @@ import java.math.BigDecimal;
 
 public interface Animal {
 
+    /**
+     * Метод, возвращающий породу
+     * @return String
+     */
     public String getBreed();
 
+    /**
+     * Метод, возвращающий имя
+     * @return String
+     */
     public String getName();
 
+    /**
+     * Метод, возвращающий стоимость
+     * @return BigDecimal
+     */
     public BigDecimal getCost();
 
+    /**
+     * Метод, возвращающий характер
+     * @return String
+     */
     public String getCharacter();
 
 }
