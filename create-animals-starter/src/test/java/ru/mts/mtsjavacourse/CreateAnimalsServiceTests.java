4b450a77eb3b3c823ca7319f1f03468f8a597df9
NikolaySimakov <n.simakoff@gmail.com>
2024-02-27 10:04:30 +0300

    Remove context

--- create-animals-starter/src/test/java/ru/mts/mtsjavacourse/CreateAnimalsServiceTests.java
+++ create-animals-starter/src/test/java/ru/mts/mtsjavacourse/CreateAnimalsServiceTests.java
@@ -4,13 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationContext;
 import ru.mts.mtsjavacourse.models.AbstractAnimal;
 import ru.mts.mtsjavacourse.models.animals.Wolf;
-import ru.mts.mtsjavacourse.services.CreateAnimalService;
 import ru.mts.mtsjavacourse.services.CreateAnimalServiceImpl;
 
 import java.math.BigDecimal;
@@ -25,8 +22,8 @@ public class CreateAnimalsServiceTests {
 
     @MockBean
     private CreateAnimalServiceImpl createAnimalServiceMock;
-    @Autowired
-    ApplicationContext context;
+//    @Autowired
+//    ApplicationContext context;
 
     @Test
     @DisplayName("Animal creation test")
@@ -34,12 +31,12 @@ public class CreateAnimalsServiceTests {
         AbstractAnimal wolfMock = new Wolf("breed", "name", new BigDecimal("123.123"), "character", LocalDate.now());
         when(createAnimalServiceMock.createAnimal(0)).thenReturn(wolfMock);
 
-        CreateAnimalService createAnimalServiceContext = context.getBean(CreateAnimalServiceImpl.class);
-        AbstractAnimal wolf = createAnimalServiceContext.createAnimal(0);
+//        CreateAnimalService createAnimalServiceContext = context.getBean(CreateAnimalServiceImpl.class);
+//        AbstractAnimal wolf = createAnimalServiceContext.createAnimal(0);
 
-        assertSame(createAnimalServiceMock.createAnimal(0).getClass(), wolf.getClass());
-        assertSame(createAnimalServiceMock.createAnimal(0), wolf);
-        System.out.println(wolf.shortInfo());
+        assertSame(createAnimalServiceMock.createAnimal(0).getClass(), wolfMock.getClass());
+        assertSame(createAnimalServiceMock.createAnimal(0), wolfMock);
+        System.out.println(wolfMock.shortInfo());
     }
 
 }
