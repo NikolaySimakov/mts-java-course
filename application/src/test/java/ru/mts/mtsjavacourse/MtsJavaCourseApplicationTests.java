635842393df9c25d4bed2a60015ae156495fae94
NikolaySimakov <n.simakoff@gmail.com>
2024-03-12 00:17:10 +0300

    Add tests for additional methods

--- application/src/test/java/ru/mts/mtsjavacourse/MtsJavaCourseApplicationTests.java
+++ application/src/test/java/ru/mts/mtsjavacourse/MtsJavaCourseApplicationTests.java
@@ -6,10 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import ru.mts.mtsjavacourse.models.AbstractAnimal;
+import ru.mts.mtsjavacourse.models.animals.Cat;
 import ru.mts.mtsjavacourse.properties.AnimalsProperties;
 import ru.mts.mtsjavacourse.repositories.AnimalsRepository;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -80,6 +85,87 @@ class MtsJavaCourseApplicationTests {
 							!animalsProperties.getWolfNames().contains(name)
 			);
 		}
+	}
+
+	@Test
+	@DisplayName("Find average age test")
+	public void testFindAverageAge() {
+		List<LocalDate> birthDates = new ArrayList<>(List.of(
+				LocalDate.of(2016, 7, 12),
+				LocalDate.of(2014, 10, 6),
+				LocalDate.of(2018, 12, 9),
+				LocalDate.of(2017, 9, 5)
+		));
+
+		List<AbstractAnimal> cats = new ArrayList<>();
+		double expected = 0;
+
+        for (LocalDate birthDate : birthDates) {
+            cats.add(new Cat("breed", "name", new BigDecimal("1.123"), "character", birthDate));
+            expected += Period.between(birthDate, LocalDate.now()).getYears();
+        }
+
+		expected = expected / cats.size();
+		double actualAverageAge = animalsRepository.findAverageAge(cats);
+		assertEquals(actualAverageAge, expected);
+	}
+
+	@Test
+	@DisplayName("Find old and expensive")
+	public void testFindOldAndExpensive() {
+		List<AbstractAnimal> cats = new ArrayList<>();
+		cats.add(new Cat("breed", "name", new BigDecimal("1.123"), "character",
+				LocalDate.of(2016, 7, 12)));
+		cats.add(new Cat("breed", "name", new BigDecimal("1"), "character",
+				LocalDate.of(2016, 3, 23)));
+		cats.add(new Cat("breed", "name", new BigDecimal("1"), "character",
+				LocalDate.of(2020, 9, 12)));
+		cats.add(new Cat("breed", "name", new BigDecimal("0.9837"), "character",
+				LocalDate.of(2021, 1, 1)));
+		cats.add(new Cat("breed", "name", new BigDecimal("0.8"), "character",
+				LocalDate.of(2022, 6, 1)));
+
+		List<AbstractAnimal> expectedCats = new ArrayList<>(List.of(
+				new Cat("breed", "name", new BigDecimal("1"), "character",
+						LocalDate.of(2016, 3, 23)),
+				new Cat("breed", "name", new BigDecimal("1.123"), "character",
+						LocalDate.of(2016, 7, 12))
+		));
+
+		List<AbstractAnimal> actualCats = animalsRepository.findOldAndExpensive(cats);
+
+		assertNotNull(actualCats);
+		assertEquals(actualCats, expectedCats);
+	}
+
+	@Test
+	@DisplayName("Find min cost animals")
+	public void testFindMinCostAnimals() {
+		List<AbstractAnimal> cats = new ArrayList<>();
+		cats.add(new Cat("breed1", "A", new BigDecimal("1.2"), "character",
+				LocalDate.of(2016, 7, 12)));
+		cats.add(new Cat("breed2", "B", new BigDecimal("1.1"), "character",
+				LocalDate.of(2016, 3, 23)));
+		cats.add(new Cat("breed3", "C", new BigDecimal("1"), "character",
+				LocalDate.of(2020, 9, 12)));
+		cats.add(new Cat("breed4", "D", new BigDecimal("0.9837"), "character",
+				LocalDate.of(2021, 1, 1)));
+		cats.add(new Cat("breed5", "E", new BigDecimal("0.8"), "character",
+				LocalDate.of(2022, 6, 1)));
+
+		List<AbstractAnimal> actualCats = animalsRepository.findMinConstAnimals(cats);
+
+		List<AbstractAnimal> expectedCats = new ArrayList<>(List.of(
+				new Cat("breed5", "E", new BigDecimal("0.8"), "character",
+						LocalDate.of(2022, 6, 1)),
+				new Cat("breed4", "D", new BigDecimal("0.9837"), "character",
+						LocalDate.of(2021, 1, 1)),
+				new Cat("breed3", "C", new BigDecimal("1"), "character",
+						LocalDate.of(2020, 9, 12))
+		));
+
+		assertNotNull(actualCats);
+		assertEquals(actualCats, expectedCats);
 	}
 
 }
