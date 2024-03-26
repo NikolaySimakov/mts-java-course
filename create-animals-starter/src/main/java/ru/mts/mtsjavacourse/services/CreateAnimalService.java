d7261fea8f8eb5ab727ef9fc981bb6feea457a0c
NikolaySimakov <n.simakoff@gmail.com>
2024-03-26 19:06:43 +0300

    Update CreateAnimalService default method

--- create-animals-starter/src/main/java/ru/mts/mtsjavacourse/services/CreateAnimalService.java
+++ create-animals-starter/src/main/java/ru/mts/mtsjavacourse/services/CreateAnimalService.java
@@ -4,14 +4,18 @@ import ru.mts.mtsjavacourse.models.AbstractAnimal;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public interface CreateAnimalService {
 
     AbstractAnimal createAnimal(int i);
 
     default Map<String, List<AbstractAnimal>> createAnimals() {
-        Map<String, List<AbstractAnimal>> animalsMap = new HashMap<>();
+        Map<String, List<AbstractAnimal>> animalsMap = new ConcurrentHashMap<>();
         AnimalFactory animalFactory = new AnimalFactory();
         Random random = new Random();
         int countOfAnimals = 0;
@@ -27,7 +31,9 @@ public interface CreateAnimalService {
             if (animalsMap.containsKey(key)) {
                 animalsMap.get(key).add(animal);
             } else {
-                animalsMap.put(key, new ArrayList<AbstractAnimal>());
+                List<AbstractAnimal> animalList = new CopyOnWriteArrayList<>();
+                animalList.add(animal);
+                animalsMap.put(key, animalList);
             }
 
             countOfAnimals++;
